ac7068f5522a449067712ca0b8f0db5e719fabcc
Dan Guest <dguest@cern.ch>
2017-06-09 20:46:55 +0200

    modify setup and run scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ cd WorkArea/run
 Reco_tf.py \
     --preExec 'from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-29";BTaggingFlags.ForceMV2CalibrationAlias=False;' \
     --inputAODFile \
-    /afs/cern.ch/work/n/nwhallon/public/xAOD_samples/mc15_13TeV.301501.MadGraphPythia8EvtGen_A14NNPDF23LO_RS_G_hh_bbbb_c10_M1600.merge.AOD.e3820_s2608_s2183_r7772_r7676/AOD.08176602._000006.pool.root.1 \
-           --outputDAODFile FTAG5.pool.root \
-           --maxEvents 10 \
-           --reductionConf FTAG5 | tee log_FTAG5.dat
+    /afs/cern.ch/work/d/dguest/public/mc16_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.AOD.e3698_s2997_r8903_r8906/AOD.10226638._000222.pool.root.1 \
+    --outputDAODFile FTAG5.pool.root \
+    --maxEvents 10 \
+    --reductionConf FTAG5 | tee log_FTAG5.dat
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
 # setup ATLAS
-setupATLAS
-lsetup asetup
-asetup 20.7.8.4,AtlasDerivation,here
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+echo -n "setting up local ATLAS environment..."
+setupATLAS -q
+lsetup asetup -q
+
+. $AtlasSetup/scripts/asetup.sh 21.0.11.1,AtlasDerivation,here > /dev/null
+echo "done"
+
